fba8bf99d582cb2fea4eb936f4764cda72d16d8a
Felipe de Paula Santos <felipedpaulasantos@gmail.com>
2020-07-27 16:40:05 -0300

    Função reset

--- index.js
+++ index.js
@@ -3,26 +3,34 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 var count = 0;
-
-app.get('/', (req, res) => {
-	res.send('<h1>App funcionando!</h1>');
-});
+var updatedCount = "App funcionando!";
 
 io.on('connection', (socket) => {
 	console.log('a user connected');
 
 	var teste = setInterval(() => {
+		if (count == 100) { count = 0; }
 		io.emit('my broadcast', `${count}`);
 		count = count + 1;
 	}, 3000);
+
+	socket.on('reset', () => {
+		console.log("Resetando...");
+		count = 0;
+	});
 
 	socket.on('disconnect', () => {
 		console.log('user disconnected');
 	});
 
 	socket.on('my message', (msg) => {    
-		console.log('message: ' + msg);  
+		updatedCount = msg;
+		console.log(msg);  
 	});
+});
+
+app.get('/', (req, res) => {
+	res.send(`<h1>${updatedCount}</h1>`);
 });
 
 http.listen(process.env.PORT || 3000, () => {

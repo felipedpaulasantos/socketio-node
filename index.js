49e53dd9320b7af353b56252ace5730c22c41f9a
Felipe de Paula Santos <felipedpaulasantos@gmail.com>
2020-07-27 15:49:15 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+var count = 0;
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hey Socket.io</h1>');
+});
+
+io.on('connection', (socket) => {
+	console.log('a user connected');
+
+	var teste = setInterval(() => {
+		io.emit('my broadcast', `${count}`);
+		count = count + 1;
+	}, 3000);
+
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+	});
+
+	socket.on('my message', (msg) => {    
+		console.log('message: ' + msg);  
+	});
+});
+
+http.listen(3000, () => {
+	console.log('listening on *:3000');
+});
